281fd2b34b32f61c2aeb7f55c02ea06df8fa1efe
Tdude <tibbemail@gmail.com>
2025-04-16 13:13:55 +0200

    Some things got lost in stricter typing but now theyre back

--- src/components/forms/evaluation/StepByStepView.tsx
+++ src/components/forms/evaluation/StepByStepView.tsx
@@ -4,13 +4,14 @@
 import React, { useRef } from 'react';
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight, List } from 'lucide-react';
-import { FormData, Question } from '@/lib/types/formTypesEvaluation';
+import { FormData, QuestionsStructure, Question } from '@/lib/types/formTypesEvaluation';
 import QuestionCard from '@/components/ui/evaluation/QuestionCard';
-import DualSectionProgressBar from '@/components/ui/evaluation/ProgressBar';
+import { DualSectionProgressBar } from '@/components/ui/evaluation/ProgressBar';
 import LoadingDots from '@/components/ui/LoadingDots';
 
 interface StepByStepViewProps {
   formData: FormData;
+  questionsStructure: QuestionsStructure;
   allQuestions: Array<{
     sectionId: keyof FormData;
     questionId: string;
@@ -38,6 +39,7 @@ interface StepByStepViewProps {
  */
 const StepByStepView: React.FC<StepByStepViewProps> = ({
   formData,
+  questionsStructure,
   allQuestions,
   currentQuestionIndex,
   currentSection,
